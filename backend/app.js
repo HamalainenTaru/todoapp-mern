3958fe655a9be5f44d2d0d62928540a266a24c53
HamalainenTaru <thamalainen1990@gmail.com>
2024-04-28 19:20:30 +0300

    connecting to mongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,25 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const config = require("./utils/config");
+const logger = require("./utils/logger");
 
 const app = express();
+
+//#region Connecting to MongoDB
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(config.MONGO_URL)
+  .then(() => {
+    logger.info("Succesfully connected to MongoDB");
+  })
+  .catch((error) => {
+    logger.error("Failed connecting to MongoDB", error.message);
+  });
+//#endregion
+
+app.use(express.json());
+app.use(cors());
 
 module.exports = app;
